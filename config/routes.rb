cdfd3904d1349f8a72d7db2665cdffb17e946580
Taylor Mravic <taylor.mravic@gmail.com>
2022-02-26 16:38:17 +0900

    demo complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,20 @@ Rails.application.routes.draw do
   # member
   resources :restaurants do
     # /restaurants/...
-    
+
     collection do
       # get 'top', to: 'restaurants#top', as: :top
       get :top
     end
 
+    member do
+      get :chef
+    end
 
+    resources :reviews, only: [ :new, :create ]
   end
+  resources :reviews, only: [ :destroy ]
+  # not nested because it doesn't need the restaurant_id from the url
+  # you only need the review_id, /reviews/:id
+  # it just goes to the controller and calls @restaurant.destroy
 end
